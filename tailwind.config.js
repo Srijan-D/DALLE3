33a77e63f843829dc48b1d15c77327c4c2f16146
srijan <srijandubey3105@gmail.com>
2023-03-25 14:34:07 +0530

    configuring tailwind v2 into nextjs13.2

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
