da2c90c2381355d25130a5877b09db3abbdb2c65
srijan <srijandubey3105@gmail.com>
2023-03-26 18:10:24 +0530

    fixed error getting blob's text response from the api

--- app/api/getImages/route.ts
+++ app/api/getImages/route.ts
@@ -1,11 +1,9 @@
-import { NextResponse } from "next/server";
-
 export async function GET(request: Request) {
     const response = await fetch("http://localhost:7071/api/getImages", {
         cache: "no-store",
     })
     const blob = await response.blob();
-    const textData = await response.text();
+    const textData = await blob.text();
 
     const data = JSON.parse(textData);
 
