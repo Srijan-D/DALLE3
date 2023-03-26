83a8fb27adb43ea991d22f257d7596a80d3fbe0e
srijan <srijandubey3105@gmail.com>
2023-03-26 19:42:53 +0530

    fetching data client side along using useSWR hook for caching of images

---
+++ components/Images.tsx
@@ -0,0 +1,58 @@
+"use client"
+import Image from "next/image"
+import useSWR from "swr"
+import fetchImages from "../lib/fetchImages"
+
+type ImageType = {
+    name: string
+    url: string
+}
+
+function Images() {
+    const {
+        data: images,
+        isLoading,
+        mutate: refreshImages,
+        isValidating,
+    } = useSWR('/api/getImages', fetchImages, {
+        revalidateOnFocus: false,
+    });
+    console.log(images)
+
+    return <div >
+        <button onClick={()=>refreshImages(images)}>
+            {/* images is passed inside for optimistic updation */}
+            {!isLoading && isValidating ? "Refreshing..." : "Refresh Images"}
+        </button>
+
+        {isLoading && (
+            <p className="text-center pb-8 font-extralight">
+                Loading <span className="text-violet-400">AI</span> Generated Images...
+            </p>
+        )}
+
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-0 md:px-10">
+            {images?.imageUrls?.map((image: ImageType, num: Number) => (
+                <div key={image.name} className={`relative cursor-help 
+                ${num === 0 && "md:col-span-2 md:row-span-2"}
+                hover:scale-[104%] transition transform duration-300 ease-in-out
+                `}> {/* div for hovering effect like in DALLE.2 */}
+                    <div className="absolute flex justify-center items-center w-full h-full bg-white opacity-0 hover:opacity-80 transition-opacity duration-200 z-10">
+                        <p className="text-center font-light text-lg p-5">
+                            {image.name.split("_").shift()?.toString().split(".").shift()}
+                        </p>
+                    </div>
+                    <Image
+                        src={image.url}
+                        alt={image.name}
+                        width={500}
+                        height={500}
+                        className="w-full rounded-sm shadow-2xl drop-shadow-lg z-10"
+                    />
+                </div>
+            ))}
+        </div>
+    </div>
+}
+
+export default Images
