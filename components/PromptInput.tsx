315c99570fff874cc346059940154bcdc6443fc8
srijan <srijandubey3105@gmail.com>
2023-03-26 16:15:00 +0530

    connecting client side to DALL.E 2 client and with blob storage

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -9,9 +9,32 @@ function PromptInput() {
     })
     // console.log(suggestion, isLoading)
     const loading = isLoading || isValidating
+
+    const submitPrompt = async (useSuggestion?: boolean) => {
+        const inputPrompt = input
+        setInput("")
+        console.log(inputPrompt)
+
+        const p = useSuggestion ? suggestion : inputPrompt
+
+        const res = await fetch('/api/generateImage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ prompt: p })
+        })
+        const data = await res.json()
+
+    }
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        await submitPrompt();
+    }
+
     return (
         <div className="m-10">
-            <form className="flex flex-col lg:flex-row lg:divide-x-2 shadow-md shadow-slate-400/10 rounded-md">
+            <form onSubmit={handleSubmit} className="flex flex-col lg:flex-row lg:divide-x-2 shadow-md shadow-slate-400/10 rounded-md">
                 <textarea className="flex-1 outline-none rounded-md p-4"
                     placeholder={(loading && "ChatGPT is thinking of a suggestion...") || suggestion || "Enter a prompt"}
                     value={input}
@@ -26,7 +49,7 @@ function PromptInput() {
 
                 <button className="p-4 bg-violet-400 text-white  transition-colors duration-200 font-bold disabled:bg-gray-400 disabled:cursor-not-allowed
                 disable:text-gray-300
-                " type="button">Use suggestion</button>
+                " type="button" onClick={() => submitPrompt(true)}>Use suggestion</button>
 
                 <button type="button"
                     className="text-violet-400 bg-white p-4 transition-colors duration-200 rounded-b-md font-bold
