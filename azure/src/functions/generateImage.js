c56dd376ea89eed229a6c8feef2cfc5e24c4b3fb
srijan <srijandubey3105@gmail.com>
2023-03-26 15:04:12 +0530

    established connection between blob container and DALL.E api to fetch api urls and downloading it on blob storage So, if the open ai servers go down image is accessible

---
+++ azure/src/functions/generateImage.js
@@ -0,0 +1,47 @@
+const { app } = require('@azure/functions');
+const openai = require('../../lib/openai');
+const axios = require('axios');
+const generateSASToken = require('../../lib/generateSASToken');
+
+const { BlobServiceClient } = require('@azure/storage-blob');
+
+const accountName = process.env.accountName;
+const containerName = "images";
+
+app.http("generateImage", {
+    method: ["POST"],
+    authLevel: "anonymous",
+    handler: async (request, context) => {
+        const { prompt } = await request.body;
+        console.log("PROMPT=>", prompt);
+
+        const response = await openai.createImage({
+            prompt: prompt,
+            n: 1,//number of images to generate
+            size: '1024x1024'
+        })
+        image_url = response.data.data[0].url;
+
+        //now we need to download the image and upload it to azure blob storage
+        const res = await axios.get(image_url, { responseType: 'arraybuffer' });
+        //arraybuffer is a binary representation of the image used to store the image in memory
+        const arrayBuffer = res.data;
+
+        sasToken = await generateSASToken();
+
+        //getting access to the storage account  
+        const blobServiceClient = new BlobServiceClient(
+            `https://${accountName}.blob.core.windows.net?${sasToken}`
+        )
+        //getting access to the container inside the storage account
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+
+        //generating time stamp( for being able to display it according to the time) and filename for the image
+        const timestamp = new Date().getTime();
+        const file_name = `${prompt}_${timestamp}.png`;
+
+        //getting access to the blob inside the container
+        const blockBlobClient = containerClient.getBlockBlobClient(file_name);
+
+    }
+})
