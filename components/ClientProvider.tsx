56a34805049c8d0b4716e0fc2ea85148f5fef442
srijan <srijandubey3105@gmail.com>
2023-03-26 20:14:38 +0530

    adding react-hot-toast client side

---
+++ components/ClientProvider.tsx
@@ -0,0 +1,14 @@
+"use client"
+import{ Toaster } from "react-hot-toast"
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <>
+            <Toaster position="bottom-center"/>
+            {children}        
+        </>
+    )
+}
