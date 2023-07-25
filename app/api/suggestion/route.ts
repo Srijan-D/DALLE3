e379564d32088bff88cefe42e668ca0d57e30eb4
srijan <srijandubey3105@gmail.com>
2023-07-25 22:02:54 +0530

    cahnging azure function

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -1,6 +1,6 @@
 export async function GET(request: Request) {
     //Now our API communicates with Microsoft Azure Function Endpoint 
-    const response = await fetch("https://image-generator2.azurewebsites.net/api/getChatGPTSuggestion?", {
+    const response = await fetch("https://image-generator2.azurewebsites.net/api/getchatgptsuggestion?", {
         cache: 'no-store',
     })
 
