75e90fa5f396646e9d5ffd68509ad8892d08c4d2
Srijan-D <srijandubey3105@gmail.com>
2024-01-09 15:54:09 +0530

    adding vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
-import '../styles/globals.css'
-import Header from '@/components/Header'
-import PromptInput from '@/components/PromptInput'
-import ClientProvider from '../components/ClientProvider'
+import "../styles/globals.css";
+import Header from "@/components/Header";
+import PromptInput from "@/components/PromptInput";
+import ClientProvider from "../components/ClientProvider";
+import { Analytics } from "@vercel/analytics/react";
 export const metadata = {
-  title: 'VisionaryAI - AI image generator',
-  description: 'AI image generator Powered by Next.js, DALL-E & ChatGPT',
-}
+  title: "VisionaryAI - AI image generator",
+  description: "AI image generator Powered by Next.js, DALL-E & ChatGPT",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -19,8 +20,9 @@ export default function RootLayout({
           <Header />
           <PromptInput />
           {children}
+          <Analytics />
         </ClientProvider>
       </body>
     </html>
-  )
+  );
 }
--- azure/src/functions/getChatGPTSuggestion.js
+++ azure/src/functions/getChatGPTSuggestion.js
@@ -7,7 +7,7 @@ app.http('getChatGPTSuggestion', {
     handler: async (request, context) => {
         const response = await openai.createCompletion({
             model: 'gpt-3.5-turbo-instruct',
-            prompt: 'Write a random text prompt for DALL.E2 to generate an image, this prompt will be shown to the user,include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic,4K, abstract, modern, black and white etc. Do not wrap the answer in quotes',
+            prompt: 'Write a random text prompt for DALL.E to generate an image, this prompt will be shown to the user,include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic,4K, abstract, modern, black and white etc. Do not wrap the answer in quotes',
             max_tokens: 100,
             temperature: 0.9,
         })
